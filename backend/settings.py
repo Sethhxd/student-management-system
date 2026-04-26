02fa1610a6cac0daf359ec7696efe309cf2d28da
PUTHEAPISETH KEAN <kean.putheapiseth.1223@rupp.edu.kh>
2026-04-26 20:07:09 +0700

    Fix DATABASES

--- backend/settings.py
+++ backend/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'student_mis'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'student_mis',
+        'USER': 'student_admin',
+        'PASSWORD': 'studentadmin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
